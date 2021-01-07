76e43803a6dcd32f916f91543d7703ee7f46b691
Crummorn <Igorcaze@gmail.com>
2021-01-07 12:23:52 -0300

    Feat: Transaction Delete.

--- backend/src/services/DeleteTransactionService.ts
+++ backend/src/services/DeleteTransactionService.ts
@@ -1,9 +1,28 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+    id: string;
+}
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
-  }
+    public async execute({ id }: Request): Promise<void> {
+        const transactionRepository = getCustomRepository(
+            TransactionsRepository,
+        );
+
+        const transaction = await transactionRepository.findOne({
+            where: { id },
+        });
+
+        if (!transaction) {
+            throw new AppError('No transactions were found with this id.');
+        }
+
+        await transactionRepository.remove(transaction);
+    }
 }
 
 export default DeleteTransactionService;
