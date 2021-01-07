3b071eae943d9ce6ffea78714f65fc9edaa765e0
Crummorn <Igorcaze@gmail.com>
2021-01-07 18:54:54 -0300

    Feat: Transaction Import.

--- backend/src/services/ImportTransactionsService.ts
+++ backend/src/services/ImportTransactionsService.ts
@@ -1,9 +1,70 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+import path from 'path';
+import uploadConfig from '../config/upload';
 import Transaction from '../models/Transaction';
+import CreateTransactionService from './CreateTransactionService';
+
+interface Request {
+    fileName: string;
+}
+
+interface CSVTransaction {
+    title: string;
+    value: number;
+    type: 'income' | 'outcome';
+    category: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
-  }
+    async execute({ fileName }: Request): Promise<Transaction[]> {
+        const csvFilePath = path.join(uploadConfig.directory, fileName);
+
+        const readCSVStream = fs.createReadStream(csvFilePath);
+
+        const parseStream = csvParse({
+            from_line: 2,
+            ltrim: true,
+            rtrim: true,
+        });
+
+        const parseCSV = readCSVStream.pipe(parseStream);
+
+        const transactionsCSV: CSVTransaction[] = [];
+
+        parseCSV.on('data', async line => {
+            const [title, type, value, category] = line.map((cell: string) => {
+                return cell.trim();
+            });
+
+            if (!title || !type || !value) return;
+
+            transactionsCSV.push({ title, type, value, category });
+        });
+
+        await new Promise(resolve => {
+            parseCSV.on('end', resolve);
+        });
+
+        await fs.promises.unlink(csvFilePath);
+
+        const createTransactionService = new CreateTransactionService();
+
+        const transactions = transactionsCSV.map(
+            async ({ title, value, type, category }) => {
+                const transaction = await createTransactionService.execute({
+                    title,
+                    type,
+                    value,
+                    category,
+                });
+
+                return transaction;
+            },
+        );
+
+        return Promise.all(transactions);
+    }
 }
 
 export default ImportTransactionsService;
