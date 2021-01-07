6fca2f2ba0bf8837d03cce385aaf6e5ecfd88271
Crummorn <Igorcaze@gmail.com>
2021-01-07 11:42:51 -0300

    Feat: Transaction GetBalance

--- backend/src/repositories/TransactionsRepository.ts
+++ backend/src/repositories/TransactionsRepository.ts
@@ -3,16 +3,34 @@ import { EntityRepository, Repository } from 'typeorm';
 import Transaction from '../models/Transaction';
 
 interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+    income: number;
+    outcome: number;
+    total: number;
 }
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  public async getBalance(): Promise<Balance> {
-    // TODO
-  }
+    public async getBalance(): Promise<Balance> {
+        const balance: Balance = {
+            income: 0,
+            outcome: 0,
+            total: 0,
+        };
+
+        const transactions = await this.find();
+
+        transactions.forEach(transaction => {
+            if (transaction.type === 'income') {
+                balance.income += Number(transaction.value);
+            } else {
+                balance.outcome += Number(transaction.value);
+            }
+        });
+
+        balance.total = balance.income - balance.outcome;
+
+        return balance;
+    }
 }
 
 export default TransactionsRepository;
