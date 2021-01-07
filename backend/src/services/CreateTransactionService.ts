874b761c3f4db8ffe63f43e62972d1695542b028
Crummorn <Igorcaze@gmail.com>
2021-01-07 02:14:02 -0300

    Feat: CreateTransaction.

--- backend/src/services/CreateTransactionService.ts
+++ backend/src/services/CreateTransactionService.ts
@@ -1,11 +1,46 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { getRepository } from 'typeorm';
+import Category from '../models/Category';
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
 
+interface Request {
+    title: string;
+    value: number;
+    type: 'income' | 'outcome';
+    category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
-  }
+    public async execute({
+        title,
+        value,
+        type,
+        category: categoryTitle,
+    }: Request): Promise<Transaction> {
+        const transactionRepository = getRepository(Transaction);
+        const categoryRepository = getRepository(Category);
+
+        let category = await categoryRepository.findOne({
+            where: { title: categoryTitle },
+        });
+
+        if (!category) {
+            category = categoryRepository.create({ title: categoryTitle });
+            await categoryRepository.save(category);
+        }
+
+        const transaction = transactionRepository.create({
+            title,
+            value,
+            type,
+            category,
+        });
+
+        await transactionRepository.save(transaction);
+
+        return transaction;
+    }
 }
 
 export default CreateTransactionService;
